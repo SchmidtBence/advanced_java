523a791e07bdeaebb06487a27311f43bf2f529ec
SchmidtBence <90318140+SchmidtBence@users.noreply.github.com>
2022-04-25 01:34:59 +0200

    Add files via upload

---
+++ first/src/main/java/org/example/Operands.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Operands {
+    private Integer a;
+    private Integer b;
+
+    public Operands(){
+
+    }
+
+    public Operands(Integer a, Integer b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Integer getA() {
+        return a;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
+
+    public Integer getB() {
+        return b;
+    }
+
+    public void setB(Integer b) {
+        this.b = b;
+    }
+}
