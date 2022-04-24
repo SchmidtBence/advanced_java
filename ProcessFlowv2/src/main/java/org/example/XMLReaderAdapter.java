c08691dd7755f7844c5c5ae16174bce35882fa28
SchmidtBence <90318140+SchmidtBence@users.noreply.github.com>
2022-04-25 01:55:27 +0200

    Delete XMLReaderAdapter.java

--- ProcessFlowv2/src/main/java/org/example/XMLReaderAdapter.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-public interface XMLReaderAdapter {
-    void setIncludePath(String inputFilePath) throws FileNotFoundException;
-    String[] readNext() throws IOException;
-}
